4a2d4cad2099f537ac51f0e4f6bd8b35f2d2d6e6
GasparKral <147488938+GasparKral@users.noreply.github.com>
2024-04-19 20:21:00 +0200

    soft updates

--- src/data/ProyectsData.ts
+++ src/data/ProyectsData.ts
@@ -12,7 +12,7 @@ export const ProyectsData: ProyectsData[] = [
         title: 'TheRealColor',
         description:
             'A web app that allows you to create palettes, saved and export to vanilla CSS or Tailwind',
-        codeURL: '',
+        codeURL: 'https://github.com/GasparKral/TheRealColor.v2',
         liveURL: '',
         img: './ThRealColorthumb.webp',
         technologies: ['Express', 'React', 'Vite', 'Tailwind'],
@@ -21,7 +21,7 @@ export const ProyectsData: ProyectsData[] = [
         title: 'Scomponents',
         description:
             'A collection of reusable components for React, Astro and Vanilla JS/HTML',
-        codeURL: '',
+        codeURL: 'https://github.com/GasparKral/Scomponents',
         liveURL: '',
         img: './ScomponentsThumb.webp',
         technologies: ['React', 'Astro', 'Tailwind'],
@@ -30,7 +30,7 @@ export const ProyectsData: ProyectsData[] = [
         title: 'LMweb',
         description:
             'A blog made with Astro, React and Tailwind explaining the Marks Language subject and CSS ',
-        codeURL: '',
+        codeURL: 'https://github.com/GasparKral/LMweb',
         liveURL: '',
         img: './LMwebThumb.webp',
         technologies: ['Astro', 'React', 'Tailwind'],
