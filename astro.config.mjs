33084fea27e134b561e4bef4ac34b14129015cd8
GasparKral <147488938+GasparKral@users.noreply.github.com>
2024-04-20 09:55:48 +0200

    could it be?

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind(), react()],
-    site: 'https://gasparkral.github.io',
-    base: '/Portfolio',
-    trailingSlash: 'always',
+    site: 'https://gasparkral.github.io/Portfolio',
+    base: '/',
+    trailingSlash: 'never',
 });
